a54154317254a65e7d169b6f0f3bf5db772e61c0
Alex <alex_johnson85@hotmail.co.uk>
2017-12-21 20:18:25 +0000

    Initial Commit

---
+++ SmartTagID/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SmartTagID
+//
+//  Created by Alex Johnson on 21/12/2017.
+//  Copyright © 2017 Alex Johnson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SmartTagID/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SmartTagID
+//
+//  Created by Alex Johnson on 21/12/2017.
+//  Copyright © 2017 Alex Johnson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
