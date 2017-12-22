891386dd6c17d51ec5de14d4e54c06a5515f1c19
Alex <alex_johnson85@hotmail.co.uk>
2017-12-22 02:12:04 +0000

    NFC real logic added

--- SmartTagID/FirstViewController.swift
+++ SmartTagID/FirstViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import CoreNFC
 
-class FirstViewController: UIViewController {
+// Reference the found NFC messages
+var nfcMessages: [[NFCNDEFMessage]] = []
 
+class FirstViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    
+    @IBOutlet weak var scannedTagID: UILabel!
+    
+    var nfcSession: NFCNDEFReaderSession!
+    var scannedTagIDText = "Tap 'Scan' button below then place tag behind top of phone to read"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scannedTagID.text = scannedTagIDText
     }
-
+    
+    @IBAction func scanButtonPressed(_ sender: UIButton) {
+        self.nfcSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        // A custom description that helps users understand how they can use NFC reader mode in your app.
+        self.nfcSession.alertMessage = "Hold item behind top of phone to detect"
+        self.nfcSession.begin()
+        self.scannedTagID.text = "Scanning..."
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        
+        self.nfcSession.alertMessage = "Item detected"
+        
+        for message in messages {
+            for record in message.records {
+                print((record.payload.advanced(by: 3) = encoding: .utf8))
+            
+            }
+        }
+    }
 }
-
